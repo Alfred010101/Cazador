e5651d9a6d2677a685d115457da081d8ff86bf09
Alfred <arista21dominguez@gmail.com>
2024-12-02 17:47:27 -0600

    Estructura base del proyecto

---
+++ Cazador/src/gui/Ventana.java
@@ -0,0 +1,27 @@
+
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Alfred
+ */
+public class Ventana extends JFrame
+{
+
+    public Ventana()
+    {
+        super("Duck Hunt 2.0");
+        setSize(200, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponets();
+    }
+
+    private void initComponets()
+    {
+        
+    }
+    
+}
---
+++ Cazador/src/hilos/Pato.java
@@ -0,0 +1,23 @@
+
+package hilos;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Alfred
+ */
+public class Pato implements Runnable
+{
+
+    public Pato(JLabel pato)
+    {
+    }
+
+    
+    @Override
+    public void run()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }    
+}
