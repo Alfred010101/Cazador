9dfc02f94a3ce5340659d1ad21c6cada5e35c395
Alfred <arista21dominguez@gmail.com>
2024-12-02 18:09:10 -0600

    Fondo de venta

--- Cazador/src/gui/Ventana.java
+++ Cazador/src/gui/Ventana.java
@@ -1,7 +1,11 @@
 
 package gui;
 
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 /**
  *
@@ -9,11 +13,13 @@ import javax.swing.JFrame;
  */
 public class Ventana extends JFrame
 {
-
+    private JLayeredPane layeredPane;
+    
     public Ventana()
     {
         super("Duck Hunt 2.0");
-        setSize(200, 500);
+        setSize(525, 480);
+        setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponets();
@@ -21,7 +27,21 @@ public class Ventana extends JFrame
 
     private void initComponets()
     {
-        
+        layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(525, 480));
+        setContentPane(layeredPane);
+        layeredPane.setLayout(null);
+
+        ImageIcon n1 = new ImageIcon("src/gui/img/fondo_fondo.png");
+        ImageIcon n2 = new ImageIcon("src/gui/img/world.png");
+
+        JLabel lblFondo1 = new JLabel(n1);
+        lblFondo1.setBounds(0, 0, n1.getIconWidth(), n1.getIconHeight());
+
+        JLabel lblFondo2 = new JLabel(n2);
+        lblFondo2.setBounds(0, 0, n2.getIconWidth(), n2.getIconHeight());
+        layeredPane.add(lblFondo1, Integer.valueOf(0));
+        layeredPane.add(lblFondo2, Integer.valueOf(2));
     }
     
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Cazador
 Proyecto de programacion paralela
+
+# Requisistos
+
+->Java 18
+
+->Netbeans 18
+
+->Pantalla con resolicion minima de (525px,480px)
+
+# IMPORTANTE 
+Al clonar el proyecto deben ir a: Propiedades del proyecto>>Build>>Compiling Y activan Compile on Save.
